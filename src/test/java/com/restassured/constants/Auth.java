aaff6d5f696aaa502f44ca133fbf1cad3baefc6f
sanhalem <santosh.halemani@publicissapient.com>
2019-10-18 11:41:42 +0530

    firs commit

---
+++ src/test/java/com/restassured/constants/Auth.java
@@ -0,0 +1,10 @@
+package com.restassured.constants;
+
+public class Auth {
+
+	// Add you auth constants here
+	public static String CUSTOMER_SECRET="";
+	public static String ACCESS_SECRET="";
+	public static String ACCESS_TOKEN="";
+	
+}
---
+++ src/test/java/com/restassured/constants/Path.java
@@ -0,0 +1,9 @@
+package com.restassured.constants;
+
+public class Path {
+
+	// Add you auth constants here
+	public static String BASE_URI="";
+	public static String STATUSES="";
+	
+}
